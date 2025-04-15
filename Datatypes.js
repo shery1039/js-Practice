85826a46246e3ad2f9366738ad0fa7ea6b8e0382
Muhammad Shaheryar Akhtar <145667191+shery1039@users.noreply.github.com>
2025-04-15 10:21:16 +0000

    Datatypesandconversion

---
+++ Datatypes.js
@@ -0,0 +1,32 @@
+"Use strict"
+let score=null
+
+console.log(typeof score)
+console.log(typeof (score))
+
+//If you want to perform operation on numbers without any doubt then
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//Point to note
+
+// "33"=> 33
+// "33abc"=> NaN
+// true=>1
+
+let isloggedIn=0
+let booleanIsloggedIn=Boolean(isloggedIn)
+console.log(booleanIsloggedIn)
+
+// "1"=> true 
+// "Empty string"=> false
+//"Any thing" => true
+
+//Convert number into string
+
+let number=33
+let convertIntoString=String(number)
+console.log(typeof convertIntoString)
+console.log(convertIntoString)
